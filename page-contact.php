476d9f8db197b04092f89acc48cb9863dcbee643
David Bjørklund <dkcookie@gmail.com>
2014-09-13 20:31:16 +0200

    fixed html in page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -8,38 +8,36 @@ Template Name: Contact
 <!-- The loop -->
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="row">
-		
-		<!-- Contact page begins -->
-		<section class="grid_8">
-			<h1> <a href="<?php echo(get_permalink()); ?>"> <?php the_title(); ?></a></h1>
-			<p><?php the_content(); ?></p>
-			<hr>
-		</section>
-		<div class="grid_4">
-			<?php 
+		<div class="grid_12">
+			<!-- Contact page begins -->
+			<div class="row"><div class="grid_12"><h1> <a href="<?php echo(get_permalink()); ?>"> <?php the_title(); ?></a></h1></div></div>
+			<div class="row">
+				<section class="grid_8">
+					<p><?php the_content(); ?></p>
+					<hr>
+				</section>
+				<div class="grid_4">
+					<?php 
 
-			$image = get_field('kontakt_img');
+					$image = get_field('kontakt_img');
 
-			if( !empty($image) ): ?>
-				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					if( !empty($image) ): ?>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
+				</div>
+			</div>	
 
-			<?php endif; ?>
-
+				<!-- Press info page begins -->
+				<section class ="grid_12">
+					<?php the_field('presseinfo'); ?>
+				</section>
 		</div>
 
-		<!-- Press info page begins -->
-		<section class ="grid_12">
-			<?php the_field('presseinfo'); ?>
-		</section>
-
-
-		<?php endwhile; ?>
-		<!-- Error message -->
-		<?php else: ?>
-			<p class="grid_12"><?php echo 'Beklager, ingen sider matcher dine kriterier.'; ?></p>
-		<?php endif; ?>
-
 	</div>
-
+	<?php endwhile; ?>
+	<!-- Error message -->
+	<?php else: ?>
+		<p class="grid_12"><?php echo 'Beklager, ingen sider matcher dine kriterier.'; ?></p>
+	<?php endif; ?>
 
 <?php get_footer();
